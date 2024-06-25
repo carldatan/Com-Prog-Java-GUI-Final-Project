207a010db8ffb418c6cbdb22d3aabe5d38bad27d
aziziziw <justinn09499407162@gmail.com>
2024-06-25 16:43:10 +0800

    Create GameDifficulty.java

---
+++ src/main/java/GameDifficulty.java
@@ -0,0 +1,16 @@
+public class GameDifficulty {
+
+	private String difficulty;
+
+	GameDifficulty(String difficulty) {
+		this.difficulty = difficulty;
+	}
+
+	public String getDifficulty() {
+		return difficulty;
+	}
+
+	public void setDifficulty(String difficulty) {
+		this.difficulty = difficulty;
+	}
+}
